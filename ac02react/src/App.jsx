d907f8cd06e701d27f05098cb0045c9d9da18a53
Hikari06 <chinen.hikari@gmail.com>
2024-04-08 12:15:08 -0300

    Ac2 React-Isabella

--- ac02react/src/App.jsx
+++ ac02react/src/App.jsx
@@ -3,6 +3,7 @@ import Banner from './Componentes/Banner/Banner'
 import Apresentacao from './Componentes/Apresentacao/Apresentacao'
 import Habilidades from './Componentes/Habilidades/Habilidades'
 import Formulario from './Componentes/Formulario/Formulario'
+import Footer from './Componentes/Footer/Footer'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
     <Apresentacao></Apresentacao>
     <Habilidades></Habilidades>
     <Formulario></Formulario>
+    <Footer></Footer>
     </>
   )
 
